3fc54493b2cc800301500464f836ee50e6687a26
amscomp <jimp@amscomp.com>
2015-09-17 13:15:20 -0700

    Update Keyboard.java

--- src/android/Keyboard.java
+++ src/android/Keyboard.java
@@ -3,7 +3,6 @@ package org.apache.cordova.labs.keyboard;
 import android.app.Activity;
 import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
-import android.view.inputmethod.InputMethodService;
 import android.view.View;
 import org.apache.cordova.*;
 import org.json.JSONArray;
