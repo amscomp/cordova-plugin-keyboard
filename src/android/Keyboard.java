c946b205cfbb52170c05bb30a0df7dd4eb479bc5
amscomp <jimp@amscomp.com>
2015-09-17 13:05:17 -0700

    Update Keyboard.java

--- src/android/Keyboard.java
+++ src/android/Keyboard.java
@@ -3,10 +3,18 @@ package org.apache.cordova.labs.keyboard;
 import android.app.Activity;
 import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.InputMethodService;
 import android.view.View;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+public class MyInputMethodService extends InputMethodService {
+    @Override
+    public boolean onEvaluateInputViewShown() {
+        return true;
+    }
+}
 
 public class Keyboard extends CordovaPlugin {
 
