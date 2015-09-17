31d0cd13f21840d29708ef06b5b73203dfd18190
amscomp <jimp@amscomp.com>
2015-09-17 14:46:10 -0700

    Update MyInputMethodService.java

--- src/android/MyInputMethodService.java
+++ src/android/MyInputMethodService.java
@@ -1,7 +1,14 @@
 package org.apache.cordova.labs.keyboard;
 
-
+import android.app.Activity;
+import android.content.Context;
+import android.view.inputmethod.InputMethodManager;
+import android.view.View;
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
 import android.content.res.Configuration;
+import android.InputMethodService;
 import android.InputMethodService;
 
 
